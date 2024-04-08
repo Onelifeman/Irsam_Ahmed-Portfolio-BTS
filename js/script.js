958fc26e4b2752649e935ddb691e4e18b129c35f
Irsam Ahmed <irsamahmed93@gmail.com>
2024-04-09 01:38:52 +0200

    Portfolio_commanté

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-//icon navbar
-
+// Sélection de l'icône de menu et de la barre de navigation
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+// Action lorsque l'icône de menu est cliquée
 menuIcon.onclick = () => {
+  // Ajoute ou supprime la classe "bx-x" pour afficher ou masquer l'icône de fermeture
   menuIcon.classList.toggle("bx-x");
+  // Ajoute ou supprime la classe "active" pour afficher ou masquer la barre de navigation sur les petits écrans
   navbar.classList.toggle("active");
 };
 
-// scroll sections
-
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+// Action de défilement des sections de la page
+let sections = document.querySelectorAll("section"); // Sélection de toutes les sections de la page
+let navLinks = document.querySelectorAll("header nav a"); // Sélection des liens de navigation dans l'en-tête
 
 window.onscroll = () => {
   sections.forEach((sec) => {
@@ -20,24 +21,23 @@ window.onscroll = () => {
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
+    // Vérification si la section est visible à l'écran
     if (top >= offset && top < offset + height) {
-      // active navbar links
+      // Ajoute la classe "active" au lien de navigation correspondant à la section visible
       navLinks.forEach((links) => {
-        links.classList.remove("active");
+        links.classList.remove("active"); // Supprime la classe "active" de tous les liens
         document
           .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
+          .classList.add("active"); // Ajoute la classe "active" au lien correspondant à la section visible
       });
     }
   });
 
-  // steacky header
-
+  // En-tête collant lors du défilement
   let header = document.querySelector("header");
-
   header.classList.toggle("sticky", window.scrollY > 100);
 
-  // remove toogle icon and navbar when click navbar links (scrool)
+  // Retire l'icône de menu et la barre de navigation lorsque les liens de navigation sont cliqués (défilement)
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
